cd9440edf29f39a90276f3ec4744c4d89535502c
ymolodtsov <ymolodtsov.appwell@ancestry.com>
2021-09-29 19:34:49 +0200

    Add all the logic

--- src/telegram_app/api.py
+++ src/telegram_app/api.py
@@ -1,8 +1,14 @@
-from pprint import pprint
+import logging
 
 from telegram_app import bot
 
+logger = logging.getLogger()
+
 
 def send_message(group_id, text):
-    print(group_id)
-    pprint(bot.send_message(int(group_id), text=text))
+    logger.info(f"Sending to: {group_id}")
+    try:
+        res = bot.send_message(int(group_id), text=text)
+        logger.info(f"Sent message to {group_id}: {res}")
+    except Exception as ex:
+        logger.error(str(ex))
