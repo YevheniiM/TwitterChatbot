3a44cd9b612e6d7544ffc6aaaab13b67733fdef9
ymolodtsov <ymolodtsov.appwell@ancestry.com>
2021-10-21 21:39:35 +0200

    Add hirefire

--- src/tasks.py
+++ src/tasks.py
@@ -2,9 +2,18 @@ import os
 
 from celery import Celery
 from django.conf import settings
+from hirefire.procs.celery import CeleryProc
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitter_chatbot.settings")
 
 app = Celery("twitter_chatbot")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+class WorkerProc(CeleryProc):
+    name = 'celery'
+    queues = ['celery']
+    app = app
+    inspect_statuses = ['active']
+    simple_queues = True
