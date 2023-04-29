44bee8c7e7f90d864292875bddaa10301a211137
yevheniimolodtsov <xmolodtsov@gmail.com>
2023-04-29 18:14:29 +0200

    Change admin page

--- src/monitoring/admin.py
+++ src/monitoring/admin.py
@@ -15,9 +15,11 @@ class TwitterMonitoringForm(forms.ModelForm):
 class TwitterMonitoringAdmin(admin.ModelAdmin):
     list_display = ('twitter_handle', 'check_rate', 'telegram_channel', 'friend_count')
     search_fields = ('twitter_handle',)
+    form = TwitterMonitoringForm
 
     def friend_count(self, obj):
         return obj.friends.count()
+
     friend_count.short_description = 'Followings Count'
 
     def save_model(self, request, obj, form, change):
--- src/twitter_chatbot/admin.py
+++ src/twitter_chatbot/admin.py
@@ -6,7 +6,6 @@ from django.utils.html import format_html
 class CustomAdminSite(admin.AdminSite):
     def index(self, request, extra_context=None):
         extra_context = extra_context or {}
-        print('!!!!>    ' * 10, flush=True)
         extra_context['custom_instructions_link'] = format_html(
             '<a href="{}" class="button">Custom Instructions</a>',
             reverse('custom_instructions')
